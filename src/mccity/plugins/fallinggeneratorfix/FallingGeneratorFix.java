73ce2cab1cabbf811da72c471a04ec04acf7aaa3
galaran <galaran.h@gmail.com>
2012-11-21 22:41:14 +0400

    * Reverted min mark time to 3 ticks

--- src/mccity/plugins/fallinggeneratorfix/FallingGeneratorFix.java
+++ src/mccity/plugins/fallinggeneratorfix/FallingGeneratorFix.java
@@ -2,7 +2,9 @@ package mccity.plugins.fallinggeneratorfix;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
@@ -15,6 +17,7 @@ import java.util.Map;
 
 public class FallingGeneratorFix extends JavaPlugin implements Listener {
 
+    /** Piston Block -> Mark (last non-canceled extend) tick */
     private final Map<Block, Long> markedPistons = new HashMap<Block, Long>();
 
     @Override
@@ -22,37 +25,55 @@ public class FallingGeneratorFix extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockPistonExtend(BlockPistonExtendEvent event) {
-        if (checkMarked(event)) return;
+        if (checkMark(event)) return;
+        if (checkDragonEgg(event, true)) return;
 
         Block piston = event.getBlock();
-
         List<Block> movingBlocks = event.getBlocks();
         for (Block curBlock : movingBlocks) {
             Material curMat = curBlock.getType();
             if (curMat == Material.SAND || curMat == Material.GRAVEL) {
                 markedPistons.put(piston, piston.getWorld().getFullTime());
                 return;
-            } else if (curMat == Material.DRAGON_EGG) {
-            	event.setCancelled(true);
-            	break;
             }
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockPistonRetract(BlockPistonRetractEvent event) {
-        checkMarked(event);
+        if (checkMark(event)) return;
+        checkDragonEgg(event, false);
     }
 
-    private boolean checkMarked(BlockPistonEvent event) {
+    /**
+     * @return piston event was canceled
+     */
+    private boolean checkMark(BlockPistonEvent event) {
         Block piston = event.getBlock();
         Long markTick = markedPistons.get(piston);
         if (markTick != null) {
             if (piston.getWorld().getFullTime() > markTick + 2) {
                 markedPistons.remove(piston);
             } else {
+                event.setCancelled(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return piston event was canceled
+     */
+    private boolean checkDragonEgg(BlockPistonEvent event, boolean isExtend) {
+        Block pistonBlock = event.getBlock();
+        BlockFace direction = isExtend ? BlockFace.UP : BlockFace.DOWN;
+        if (event.getDirection() == direction && pistonBlock.getType() == Material.PISTON_STICKY_BASE) {
+            // check for dragon egg block up to piston block
+            // [piston_base] - [air] - [solid_block] - [dragon_egg] duplicator design
+            if (pistonBlock.getRelative(0, 3, 0).getType() == Material.DRAGON_EGG) {
                 event.setCancelled(true);
                 return true;
             }
